03e0ca49df59228eee58ca07241569847d34092c
M.Talha Yıldırım <talha@MTalhas-MacBook-Pro.local>
2025-11-02 22:19:25 +0300

    Added function for duration

--- src/app.py
+++ src/app.py
@@ -6,5 +6,8 @@ app = Flask(__name__, static_folder="static")
 def home():
     return render_template("index.html")
 
+def calculate_stop_duration(arrival_time, departure_time):
+   
+    return departure_time - arrival_time
 if __name__ == "__main__":
     app.run(debug=True)
